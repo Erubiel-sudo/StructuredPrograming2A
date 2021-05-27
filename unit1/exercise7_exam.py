781f13a23d5a67ce02ee224b38f258cd0075967d
erubiel <erubieltun27gmail.com>
2021-05-27 13:31:40 -0500

    done

---
+++ unit1/exercise7_exam.py
@@ -0,0 +1,11 @@
+print ("enter widht")
+widht= int(input())
+print ("enter height")
+height= int(input())
+
+if widht == height:
+    print("your figure is a square")
+else:
+    area = widht*height
+    print("The area of the rectangle is:" + str(area))
+print("end of program")
