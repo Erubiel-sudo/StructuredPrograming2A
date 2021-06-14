9212473be47b4d7cdd29f5aec8ba4ccd00304d12
erubiel <erubieltun27gmail.com>
2021-06-14 11:33:13 -0500

    in progress

--- unit2/functionsIntro.c
+++ unit2/functionsIntro.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
-
+#include "./utils.h"
 
 int myNumber =3;
 float myFloat = 3.1416;
 char myString[255] = "Hola mundo";
 
-//Declarar y definirla
-void sayHello( char message[]   ){
-    printf( "%s\n",  message    );
+// primer metodo para funciones
+//Declarar y definirla   
+// void sayHello(   char message[]    ){
+//         printf(  "%s\n",  message    );
+// } 
+
+// Segundo metodo para funciones
+//1-. Declaro
+// void sayMyName(   char message[]    );
+
+// int addTwoNumbers( int  argumento1   , int argumento2   ); //Declarando;
+
+int main(  ){
+        sayHello(  "Hello world" ); 
+        sayMyName( "Erubiel" );
+        int myResult =     addTwoNumbers(  2, 5   );
+        printf( "myResult is equal to:  %d \n",   myResult  );
+
+        printf("%d", libInteger  );
+        return 0;
 }
 
+// Segundo metodo para funciones
+//2-. Definimos
+// void sayMyName(   char message[]    ){
+//         printf(  "%s\n",  message    );
+// }
 
 
-int main(   ){
-
-    sayHello( "Hello world" );
-
-    return 0;
-}
+// int addTwoNumbers( int  argumento1   , int argumento2   ){
+//         int result = argumento1 + argumento2;
+//         return result; 
+// }
---
+++ unit2/utils.h
@@ -0,0 +1,20 @@
+#include
+
+#define pi 3.1416
+
+
+int libInteger  =  24;
+
+
+void sayHello(  char message[]   ){
+    printf(  "%s\n", message  );
+}
+
+void sayMyName( char message[]  ){
+    printf(  "%s\n", message  );
+}
+
+int addTwoNumbers( int argumento1  , int argumento2  ){
+    int result = argumento1 + argumento2;
+    return result;
+}
