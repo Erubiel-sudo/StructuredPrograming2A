535569a3e20048b54575fc734a89dd562f5d407c
erubiel <erubieltun27gmail.com>
2021-07-01 12:52:48 -0500

    Homework1_Done

--- unit2/structures_hw1.c
+++ unit2/structures_hw1.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+struct ErubielData{
+    char *name;
+    int age;
+    char *born;
+};
+
 int main() {
-	printf("Name: Erubiel\n");
-	printf("Age: 19\n");
-	printf("Born: Nov 27, 2002\n");
+    struct ErubielData erubiel;
+
+    erubiel.name = "Erubiel Tun";
+    erubiel.age = 19;
+    erubiel.born = "Nov 27, 2002";
+
+	printf("My Name is: %s \n", erubiel.name);
+	printf("My Age is: %d \n", erubiel.age);
+	printf("My Born is: %s \n", erubiel.born);
 
 	return(0);
 }
